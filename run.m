ac3c7c9e9c338d1ce436dde6f54f9347a96e9b48
Indrek Ots <ots.indrek@gmail.com>
2015-11-16 08:53:27 +0200

    try with new dataset

--- run.m
+++ run.m
@@ -32,8 +32,8 @@ function a = testIf(x)
 
 %p = predict(y_test, sparse(X_test), model);
 
-training_m = 5000;
-data = load('data.csv');
+training_m = 20000;
+data = load('data_new.csv');
 X = data(:, 2:end);
 %X = data(:, 2:end);
 y = arrayfun(@testIf, data(:, 1));
@@ -48,38 +48,37 @@ liblinear_options = cstrcat('-s 0 ', ' -B ', num2str(B), ' -c ', num2str(C), ' -
 %liblinear_options = '-s 0 -B 1 -c 1';
 l_curve = [];
 
-for i = 1:training_m/10
-	i
-	x_iter = x_train(1:i*10, :);
-	y_iter = y_train(1:i*10);
-	model = train(y_iter, sparse(x_iter), liblinear_options);
-	yt = lgCost(model.w, [x_iter, ones(i*10, 1)], y_iter, C);
-	ycv = lgCost(model.w, [x_test, ones(size(x_test, 1), 1)], y_test, C);
-	
-	pt = mean(double(predict(y_iter, sparse(x_iter), model) == y_iter));
-	pcv = mean(double(predict(y_test, sparse(x_test), model) == y_test));
-	
-	l_curve = [l_curve; i, yt, ycv, pt, pcv];
-end
-
-figure(1);
-plot(1:training_m/10, l_curve(:, 2), 'linewidth', 2, 1:training_m/10, l_curve(:, 3), 'linewidth', 2);
-title('Learning curve for linear regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Error')
-%axis([0 13 0 150])
-
-figure(2);
-plot(1:training_m/10, l_curve(:, 4), 'linewidth', 2, 1:training_m/10, l_curve(:, 5), 'linewidth', 2);
-title('Accuracy for linear regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Accuracy')
+%for i = 1:training_m/10
+%	x_iter = x_train(1:i*10, :);
+%	y_iter = y_train(1:i*10);
+%	model = train(y_iter, sparse(x_iter), liblinear_options);
+%	yt = lgCost(model.w, [x_iter, ones(i*10, 1)], y_iter, C);
+%	ycv = lgCost(model.w, [x_test, ones(size(x_test, 1), 1)], y_test, C);
+%	
+%	pt = mean(double(predict(y_iter, sparse(x_iter), model) == y_iter));
+%	pcv = mean(double(predict(y_test, sparse(x_test), model) == y_test));
+%	
+%	l_curve = [l_curve; i, yt, ycv, pt, pcv];
+%end
+%
+%figure(1);
+%plot(1:training_m/10, l_curve(:, 2), 'linewidth', 2, 1:training_m/10, l_curve(:, 3), 'linewidth', 2);
+%title('Learning curve for linear regression')
+%legend('Train', 'Cross Validation')
+%xlabel('Number of training examples')
+%ylabel('Error')
+%%axis([0 13 0 150])
+%
+%figure(2);
+%plot(1:training_m/10, l_curve(:, 4), 'linewidth', 2, 1:training_m/10, l_curve(:, 5), 'linewidth', 2);
+%title('Accuracy for linear regression')
+%legend('Train', 'Cross Validation')
+%xlabel('Number of training examples')
+%ylabel('Accuracy')
 
 model = train(y_train, sparse(x_train), liblinear_options);
-predict(y_train, sparse(x_train), model);
-liblinear_options
+[p, a, d] = predict(y_test, sparse(x_test), model);
+sum(p == y_test)
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+%fprintf('\nProgram paused. Press enter to continue.\n');
+%pause;
